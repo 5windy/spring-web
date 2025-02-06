4f30f4a2149729041f491a35381c02654a65e7a0
5windy <5windy@naver.com>
2025-02-06 12:19:53 +0900

    Update Form : sign up page

--- src/main/resources/static/script/signup.js
+++ src/main/resources/static/script/signup.js
@@ -1,9 +1,16 @@
 document.addEventListener("DOMContentLoaded", e => {
     const form = document.querySelector("form");
+
     const inputUsername = form.querySelector("#username");
     const errorUsername = document.getElementById("error-msg-username");
+    let isValidUsername = false;
+
+    const inputPassword = form.querySelector("#password");
+    let isValidPassword = false;
+
     const inputNickname = form.querySelector("#nickname");
     const errorNickname = document.getElementById("error-msg-nickname");
+    let isValidNickname = false;
 
     inputUsername.addEventListener("change", async e => {
         const username = e.target.value;
@@ -11,10 +18,16 @@ document.addEventListener("DOMContentLoaded", e => {
         const response = await fetch(`/users/valid/username?value=${username}`);
         if(response.ok) {
             errorUsername.style.display = "block";
+            isValidUsername = false;
         } else {
             errorUsername.style.display = "none";
+            isValidUsername = true;
         }
     });
+
+    inputPassword.addEventListener("change", e => {
+        isValidPassword = true;
+    })
 
     inputNickname.addEventListener("change", async e => {
         const nickname = e.target.value;
@@ -22,16 +35,46 @@ document.addEventListener("DOMContentLoaded", e => {
         const response = await fetch(`/users/valid/nickname?value=${nickname}`);
         if(response.ok) {
             errorNickname.style.display = "block";
+            isValidNickname = false;
         } else {
             errorNickname.style.display = "none";
+            isValidNickname = true;
         }
     });
 
-    form.addEventListener("submit", e => {
+    form.addEventListener("submit", async e => {
         e.preventDefault();
 
+        if(isValidUsername && isValidPassword && isValidNickname) {
+            // form.submit();
+            // fetch 메소드로 API 호출
+            const myHeaders = new Headers();
+            myHeaders.append("Content-Type", "application/json");
 
+            const raw = JSON.stringify({
+                "username": inputUsername.value,
+                "password": inputPassword.value
+            });
 
-        // form.submit();
-    })
+            const requestOptions = {
+                method: "POST",
+                headers: myHeaders,
+                body: raw,
+                redirect: "follow"
+            };
+
+            const response = await fetch("http://localhost:8080/users/signup", requestOptions);
+            // const data = await response.json();
+            // console.log("data : " + data.message);
+
+            if(response.ok) {
+                location.href = "/users/signin";
+            } else {
+
+            }
+
+        } else {
+            alert("회원가입 양식을 모두 채워주세요.");
+        }
+    });
 })
